77584283e698cbde051fb1043c49f4cbc5d3198c
wsdlly02 <wsdlly02@hotmail.com>
2025-03-31 16:41:40 +0800

    ...

--- Nix/pkgs/thermal-data-processor.nix
+++ Nix/pkgs/thermal-data-processor.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
   src = inputs.self.legacyPackages."${system}".selfSrc;
   nativeBuildInputs = [ inputs.self.legacyPackages."${system}".haskellEnv ];
   buildPhase = ''
-    ghc -O2 -Wall -threaded -rtsopts -with-rtsopts=-N ./Haskell/thermal-data-processor.hs -o thermal-data-processor
+    ghc -O2 -Wall -threaded -rtsopts -with-rtsopts=-N -optlo-O3 -funfolding-use-threshold=32 ./Haskell/thermal-data-processor.hs -o thermal-data-processor
   '';
   installPhase = ''
     mkdir -p $out/bin
